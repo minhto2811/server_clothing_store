fe2255af4a0fe6cbb365b360b562f98a15bdaa74
Minh Tô <milkissily@gmail.com>
2023-05-11 16:24:35 +0700

    16241105

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const expressHbs = require('express-handlebars');
 require('dotenv').config();
 const path = require('path');
-const db = require('../config/db');
+const db = require('./src/config/db');
 const PORT = process.env.PORT || 3000;
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 
-const route = require('../routes');
+const route = require('./src/routes');
 
 const app = express();
 
@@ -16,29 +16,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
-app.use(express.static(path.join(__dirname,'../public')))
+app.use(express.static(path.join(__dirname,'./src/public')))
 
 app.engine('hbs', expressHbs.engine({
     extname: ".hbs",
-    layoutsDir: path.join(__dirname,'views/layouts'),
-    partialsDir: "./views/partials",
+    layoutsDir: path.join(__dirname,'./src/sources/views/layouts'),
+    partialsDir: "./src/sources/views/partials",
     defaultLayout: "main",
     helpers: {
         sum: (a) => a + 1,
       }
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'./src/sources/views'));
 
 route(app);
 
-console.log("path:",path.join(__dirname,'../public'));
+
 
 
 
 
 db.connect();
 app.listen(PORT, () => {
+    console.log("path:",path.join(__dirname,'./src/public'));
     console.log(`Server is running at http://localhost:${PORT}/product/home`);
 })
 
