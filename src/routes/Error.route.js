a48f49e81b369b508c91ed712f7ea0b01f6e3e21
Minh Tô <milkissily@gmail.com>
2023-05-12 12:12:59 +0700

    12121205

--- src/routes/Error.route.js
+++ src/routes/Error.route.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('*', (req, res) => {
-    res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+    res.redirect('api/product/home')
 });
-router.get("/", (req, res) => {
-    res.send('SERVER ON')
-})
 
 module.exports = router;
