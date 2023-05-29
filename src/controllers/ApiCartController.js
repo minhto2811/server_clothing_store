72f2d8eff0a3b9c79bb4c75189bb1b3f6fe304b2
Minh Tô <milkissily@gmail.com>
2023-05-29 11:54:11 +0700

    11541905

--- src/controllers/ApiCartController.js
+++ src/controllers/ApiCartController.js
@@ -1,5 +1,5 @@
 const Cart = require('../models/Cart');
-const User = require('../models/User');
+const Product = require('../models/Product');
 const { convertleObject } = require('../utils/convertObj');
 
 class ApiController {
@@ -11,9 +11,20 @@ class ApiController {
         cart.save().then(cart => res.json(cart)).catch(err => res.json(err));
     }
 
+
     getAll(req, res, next) {
         const id_user = req.params.id_user;
-        Cart.find({ id_user: id_user }).then(arr => res.json(arr)).catch(err => res.json(err));
+        Cart.find({ id_user: id_user }).then(arr => {
+            for (let i = 0; i < arr.length; i++) {
+                Product.findOne({ _id: arr[i].id_product })
+                    .then(product => {
+                        arr[i].name_product = product.name;
+                        arr[i].price_product = product.price;
+                        arr[i].sale_product = product.sale;
+                    })
+            }
+            res.json(arr);
+        }).catch(err => res.json(err));
     }
 
 
--- src/models/Cart.js
+++ src/models/Cart.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const Cart = new mongoose.Schema({
     id_user: { type: String, require: true },
     id_product: { type: String, require: true },
+    name_product: { type: String },
+    price_product:{ type: Number},
+    sale_product:{ type: Number},
     quantity:{ type: Number, require: true },
 }, {
     collection: "carts"
